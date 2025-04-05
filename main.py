cf4643b38e683a547ebf5e4f96a845578856f5a3
Fourn-cpu <61877173+Fourn-cpu@users.noreply.github.com>
2025-04-05 14:44:57 +0200

    Framerates

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill('black')
         pygame.display.flip()
 
-    print('Starting Asteroids!')
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
+        #Limiting framerates to 60
+        dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
